906ab59f96a095ce673ce7b7c37d9b1cb05a90d5
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2018-11-09 20:09:55 -0400

    questao 2 prova 1

---
+++ matrizString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace matrizString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[,] m = new string[2, 2];
+
+            string texto = "";
+
+            for (int i = 0; i < 2;i++){
+                for (int j = 0; j < 2; j++){
+
+                    if(j == 0) Console.WriteLine("Informe o Nome: ");
+                    if (j ==1)Console.WriteLine("Informe o Sobrenome: ");
+
+                    m[i, j] = Console.ReadLine();
+                    texto += m[i, j] + " ";
+                }
+
+                texto += "\n";
+            }
+
+            Console.WriteLine(texto);
+        }
+    }
+}
